bc4f006e8e8ccefa8930c909ca22eef17d45bcae
phxsu <mda-2004@mail.ru>
2022-12-09 11:03:25 +0500

    Добавьте файлы проекта.

---
+++ BeauteAPI/Models/Gender.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace BeauteAPI.Models
+{
+    public partial class Gender
+    {
+        public Gender()
+        {
+            Client = new HashSet<Client>();
+        }
+
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Client> Client { get; set; }
+      
+    }
+}
